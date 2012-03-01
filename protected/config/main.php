f51bbd1dd44fdd43f70e340aba42a21196c28150
Li He <cranelee@gmail.com>
2012-03-01 13:47:38 +0800

    change config file

--- protected/config/main.php
+++ protected/config/main.php
@@ -23,7 +23,7 @@ return array(
 		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'lihe',
 		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
